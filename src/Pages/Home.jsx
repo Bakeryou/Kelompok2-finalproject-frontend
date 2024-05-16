1cc19e48dd868957033da5914b5a4eee7a1b82c6
Sabina Izatul Zahrani <sabinaizatulzahrani@gmail.com>
2024-05-17 00:19:51 +0700

    Update navbar

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,13 +1,10 @@
 import React from 'react'
 import Footer from '../components/Footer'
-import NavbarPage from '../components/Navbar'
-
 
 
 const HomePage = () => {
     return (
         <>
-            <NavbarPage />
             <div className="h-screen mt-20">Home</div>
             <Footer />
         </>
