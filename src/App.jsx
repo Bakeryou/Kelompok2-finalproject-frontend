3a49531237c128918a65dd87b3298afd51e71a76
Sabina Izatul Zahrani <sabinaizatulzahrani@gmail.com>
2024-05-21 22:03:31 +0700

    Added landing page layout

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,15 @@ import HomePage from './Pages/Home'
 import MainLayout from './layouts/MainLayout';
 import Login from './Pages/Login';
 import Register from './Pages/Register';
+import LandingLayout from './layouts/LandingLayout';
 
 const App = () => {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<LandingLayout />}>
+          <Route index element={<HomePage />} />
+        </Route>
         <Route path="/" element={<MainLayout />}>
           <Route path="login" element={<Login />} />
           <Route path="register" element={<Register />} />
