9f7073a78bc349db03feb52d1bd8a57811367e4a
Sabina Izatul Zahrani <sabinaizatulzahrani@gmail.com>
2024-05-21 22:51:29 +0700

    Update Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
 
 const Header = () => {
     const [isOpen, setIsOpen] = useState(false);
     const [isUserDropdownOpen, setIsUserDropdownOpen] = useState(false);
     const [currentUser, setCurrentUser] = useState(null); // Ganti dengan data user yang sudah login
-    const [activeMenu, setActiveMenu] = useState('home');
+    const navigate = useNavigate();
+    const location = useLocation();
 
     const toggleMenu = () => {
         setIsOpen(!isOpen);
@@ -14,16 +16,21 @@ const Header = () => {
         setIsUserDropdownOpen(!isUserDropdownOpen);
     };
 
-    const handleMenuClick = (menu) => {
-        setActiveMenu(menu);
+    const handleNavigate = (path) => {
+        navigate(path);
+        setIsOpen(false);
     };
+
     return (
         <nav className="fixed top-0 z-40 w-full bg-white shadow-md start-0">
             <div className="px-2 mx-auto max-w-7xl sm:px-6 lg:px-8">
                 <div className="relative flex items-center justify-between h-16">
-                    <div className="absolute inset-y-0 left-0 flex items-center sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                    <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                         {/* Hamburger icon */}
-                        <button onClick={toggleMenu} className="inline-flex items-center justify-center p-2 text-gray-700 transition duration-150 ease-in-out rounded-md sm:hidden hover:text-gray-900 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-900">
+                        <button
+                            onClick={toggleMenu}
+                            className="inline-flex items-center justify-center p-2 text-gray-700 transition duration-150 ease-in-out rounded-md hover:text-gray-900 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-900"
+                        >
                             <svg className="w-6 h-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
                             </svg>
@@ -31,16 +38,28 @@ const Header = () => {
                     </div>
                     <div className="flex items-center justify-center flex-1 sm:justify-start">
                         <div className="flex-shrink-0">
-                            <a href="#" className="text-lg font-bold">Bakeryou</a>
+                            <a href="/" className="text-lg font-bold">Bakeryou</a>
                         </div>
                         <div className="hidden sm:flex sm:ml-auto sm:mr-auto">
                             <div className="flex justify-center space-x-8">
-                                <a href="#" className={`px-2 text-lg font-semibold rounded-xl ${activeMenu === 'home' ? 'bg-[#D8AE7E]' : 'hover:bg-[#D8AE7E]'} transition duration-150 ease-in-out`}
-                                    onClick={() => handleMenuClick('home')}>Home</a>
-                                <a href="#" className={`px-2 text-lg font-semibold rounded-xl ${activeMenu === 'products' ? 'bg-[#D8AE7E]' : 'hover:bg-[#D8AE7E]'} transition duration-150 ease-in-out`}
-                                    onClick={() => handleMenuClick('products')}>Products</a>
-                                <a href="#" className={`px-2 text-lg font-semibold rounded-xl ${activeMenu === 'cart' ? 'bg-[#D8AE7E]' : 'hover:bg-[#D8AE7E]'} transition duration-150 ease-in-out`}
-                                    onClick={() => handleMenuClick('cart')}>Cart</a>
+                                <Link
+                                    to="/"
+                                    className={`px-2 text-lg font-semibold rounded-xl ${location.pathname === '/' ? 'bg-[#D8AE7E]' : 'hover:bg-[#D8AE7E]'} transition duration-150 ease-in-out`}
+                                >
+                                    Home
+                                </Link>
+                                <Link
+                                    to="/products"
+                                    className={`px-2 text-lg font-semibold rounded-xl ${location.pathname === '/products' ? 'bg-[#D8AE7E]' : 'hover:bg-[#D8AE7E]'} transition duration-150 ease-in-out`}
+                                >
+                                    Products
+                                </Link>
+                                <Link
+                                    to="/cart"
+                                    className={`px-2 text-lg font-semibold rounded-xl ${location.pathname === '/cart' ? 'bg-[#D8AE7E]' : 'hover:bg-[#D8AE7E]'} transition duration-150 ease-in-out`}
+                                >
+                                    Cart
+                                </Link>
                             </div>
                         </div>
                     </div>
@@ -59,24 +78,32 @@ const Header = () => {
                                 {isUserDropdownOpen && (
                                     <div className="absolute right-0 w-48 mt-2 bg-white rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                                         <div className="py-1">
-                                            <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
+                                            <Link to="/profile" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
                                                 My Profile
-                                            </a>
-                                            <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
+                                            </Link>
+                                            <Link to="/orders" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
                                                 Orders
-                                            </a>
-                                            <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
+                                            </Link>
+                                            <button
+                                                onClick={() => {
+                                                    setCurrentUser(null);
+                                                    navigate('/');
+                                                }}
+                                                className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                                            >
                                                 Logout
-                                            </a>
+                                            </button>
                                         </div>
                                     </div>
                                 )}
                             </div>
                         ) : (
-                            <a href="#" className={`px-2 text-lg font-semibold rounded-xl ${activeMenu === 'login/register' ? 'bg-[#D8AE7E]' : 'hover:bg-[#D8AE7E]'} transition duration-150 ease-in-out`}
-                                onClick={() => handleMenuClick('login/register')}>
+                            <Link
+                                to="/login"
+                                className={`px-2 text-lg font-semibold rounded-xl ${location.pathname === '/login' ? 'bg-[#D8AE7E]' : 'hover:bg-[#D8AE7E]'} transition duration-150 ease-in-out`}
+                            >
                                 Login/Register
-                            </a>
+                            </Link>
                         )}
                     </div>
                 </div>
@@ -85,31 +112,68 @@ const Header = () => {
             {/* Mobile menu */}
             <div className={`${isOpen ? 'block' : 'hidden'} sm:hidden`}>
                 <div className="px-2 pt-2 pb-3 space-y-1">
-                    <a href="#" className="hover:bg-[#D8AE7E] block px-3 py-2 rounded-md text-base font-medium">Home</a>
-                    <a href="#" className="hover:bg-[#D8AE7E] block px-3 py-2 rounded-md text-base font-medium">Products</a>
-                    <a href="#" className="hover:bg-[#D8AE7E] block px-3 py-2 rounded-md text-base font-medium">Cart</a>
+                    <Link
+                        to="/"
+                        className={`hover:bg-[#D8AE7E] block px-3 py-2 rounded-md text-base font-medium ${location.pathname === '/' ? 'bg-[#D8AE7E]' : ''}`}
+                        onClick={() => handleNavigate('/')}
+                    >
+                        Home
+                    </Link>
+                    <Link
+                        to="/products"
+                        className={`hover:bg-[#D8AE7E] block px-3 py-2 rounded-md text-base font-medium ${location.pathname === '/products' ? 'bg-[#D8AE7E]' : ''}`}
+                        onClick={() => handleNavigate('/products')}
+                    >
+                        Products
+                    </Link>
+                    <Link
+                        to="/cart"
+                        className={`hover:bg-[#D8AE7E] block px-3 py-2 rounded-md text-base font-medium ${location.pathname === '/cart' ? 'bg-[#D8AE7E]' : ''}`}
+                        onClick={() => handleNavigate('/cart')}
+                    >
+                        Cart
+                    </Link>
                     {currentUser ? (
                         <>
                             <div className="border-t border-gray-300"></div>
-                            <a href="#" className="hover:bg-[#D8AE7E] block px-3 py-2 rounded-md text-base font-medium">
+                            <Link
+                                to="/profile"
+                                className={`hover:bg-[#D8AE7E] block px-3 py-2 rounded-md text-base font-medium ${location.pathname === '/profile' ? 'bg-[#D8AE7E]' : ''}`}
+                                onClick={() => handleNavigate('/profile')}
+                            >
                                 My Profile
-                            </a>
-                            <a href="#" className="hover:bg-[#D8AE7E] block px-3 py-2 rounded-md text-base font-medium">
+                            </Link>
+                            <Link
+                                to="/orders"
+                                className={`hover:bg-[#D8AE7E] block px-3 py-2 rounded-md text-base font-medium ${location.pathname === '/orders' ? 'bg-[#D8AE7E]' : ''}`}
+                                onClick={() => handleNavigate('/orders')}
+                            >
                                 Orders
-                            </a>
-                            <a href="#" className="hover:bg-[#D8AE7E] block px-3 py-2 rounded-md text-base font-medium">
+                            </Link>
+                            <button
+                                onClick={() => {
+                                    // Tambahkan logika logout di sini
+                                    setCurrentUser(null);
+                                    handleNavigate('/');
+                                }}
+                                className="hover:bg-[#D8AE7E] block w-full text-left px-3 py-2 rounded-md text-base font-medium"
+                            >
                                 Logout
-                            </a>
+                            </button>
                         </>
                     ) : (
-                        <a href="#" className="hover:bg-[#D8AE7E] block px-3 py-2 rounded-md text-base font-medium">
+                        <Link
+                            to="/login"
+                            className={`hover:bg-[#D8AE7E] block px-3 py-2 rounded-md text-base font-medium ${location.pathname === '/login' ? 'bg-[#D8AE7E]' : ''}`}
+                            onClick={() => handleNavigate('/login')}
+                        >
                             Login/Register
-                        </a>
+                        </Link>
                     )}
                 </div>
             </div>
         </nav>
-    )
-}
+    );
+};
 
-export default Header
+export default Header;
